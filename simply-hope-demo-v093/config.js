827464f226051695ef98346be3c21c06e8c552fe
trmitchell95-oss <trmitchell95@gmail.com>
2026-08-24 14:09:24 -0500

    Add Simply Hope remote demo config

---
+++ simply-hope-demo-v093/config.js
@@ -0,0 +1,12 @@
+export const CK_CONFIG = {
+  studioSlug: 'simply-hope-designs',
+  supabaseUrl: '',
+  supabasePublishableKey: '',
+  cloudRequired: false,
+  demoMode: true,
+  version: '0.9.3',
+  releaseName: 'AI Intake + Client Cards',
+  buildId: 'v0.9.3-remote-demo',
+  schemaVersion: '0006_v090_ai_intake_client_cards',
+  buildDate: '2026-08-24'
+};
